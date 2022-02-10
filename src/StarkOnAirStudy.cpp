a1b47710153d64313aba2d2dc24d63ee988d8379
Michael Starkenburg <mike@starkenburg.com>
2022-02-10 12:24:33 -0800

    updated todo list

--- src/StarkOnAirStudy.cpp
+++ src/StarkOnAirStudy.cpp
@@ -1,6 +1,5 @@
 // Stark's TinyPico OnAir Light Device 
-// todo list: Battery HW, Battery Code, Status page at root, OTA, MQTT, Deep Sleep, 
-
+// todo list: Battery Code, Status page at root, OTA, MQTT, Deep Sleep, 
 
 #include <Arduino.h>
 #include <WiFi.h>
@@ -20,16 +19,21 @@ TinyPICO tp = TinyPICO();
 #define DOTSTAR_CLK 12
 #define BAT_CHARGE 34
 #define BAT_VOLTAGE 35
+#define USB_PLUGGED 9
+bool charge = false;
+float volts = 100;
+bool plugged = true;
 
 // Init Neopixel Strip
 #define NUM_LEDS 20
 #define DATA_PIN 14
 CRGB leds[NUM_LEDS];
-
 String LED_STATE = "OFF";
 
 void setup() {
-  
+
+  pinMode(9, INPUT_PULLUP);   // to read usb plugged in state
+
 // setup FastLED Strip
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   FastLED.clear();  // clear all pixel data
@@ -37,7 +41,7 @@ void setup() {
 
 // Shutdown Onboard Dotstar
   tp.DotStar_SetPower( false );
-
+ 
 // Serial Monitor begin and Wifi Connect
   Serial.begin(115200);
   Serial.print("Connecting to ");
@@ -75,7 +79,7 @@ void loop(){
               Serial.println("Call for RED LED");
               LED_STATE = "RED";              
               for(int dot = 0; dot < NUM_LEDS; dot++) { 
-                leds[dot].setRGB( 128, 0, 0 );
+                leds[dot].setRGB( 64, 0, 0 );
               }
               FastLED.show(); 
               Serial.println("FastLed Strip RED");
@@ -84,7 +88,7 @@ void loop(){
               Serial.println("Call for LED BLUE");
               LED_STATE = "BLUE";
               for(int dot = 0; dot < NUM_LEDS; dot++) { 
-                leds[dot].setRGB( 0, 0, 128 );
+                leds[dot].setRGB( 0, 0, 64 );
               }
               FastLED.show(); 
               Serial.println("FastLed Strip BLUE");
@@ -93,7 +97,7 @@ void loop(){
               Serial.println("Call for LED GREEN");
               LED_STATE = "GREEN";
               for(int dot = 0; dot < NUM_LEDS; dot++) { 
-                leds[dot].setRGB( 0, 128, 0 );
+                leds[dot].setRGB( 0, 64, 0 );
               }
               FastLED.show(); 
               Serial.println("FastLed Strip GREEN");
@@ -107,17 +111,40 @@ void loop(){
               FastLED.show(); 
               Serial.println("FastLed cleared");
             } else {}
-// Setup HTML Page
+
+// Get Tinypico State
+            charge = tp.IsChargingBattery();
+            volts = tp.GetBatteryVoltage();
+            plugged = digitalRead(9);
+
+// Deliver HTML Page
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #4CAF50; border: 2px solid #4CAF50;; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; }");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");            
-// Web Page Heading
             client.println("</style></head>");
             client.println("<body><center><h1>Stark's OnAir Light</h1></center>");
-            client.println("<center><h2>Press on button to turn on led and off button to turn off LED</h3></center>");
+            client.println("<center><h2>Press on button to turn on led and off button to turn off LED</h3></center><br><br>");            
+            if (plugged == true) {
+              client.println("USB is CONNECTED </p>");
+              Serial.println("USB is CONNECTED");
+              if (charge == true) {
+                client.println("Battery is CHARGING </p>");
+                Serial.println("Battery is CHARGING");
+                } else {
+                client.println("Battery is IDLE </p>");
+                Serial.println("Battery is IDLE");
+                }
+              } else {
+                client.println("USB is NOT CONNECTED </p>");
+                Serial.println("USB is NOT CONNECTED");
+                client.println("Battery is DISCHARGING </p>");
+                Serial.println("Battery is DISCHARGING");
+              }
+            client.println("Current Voltage is roughly " + String(volts) + "</p>");
+            Serial.println("Current Voltage is roughly " + String(volts) + "</p>");
             client.println("<form><center>");
             client.println("<p> LED is " + LED_STATE + "</p>");
             client.println("<center> <button class=\"button\" name=\"LED\" value=\"RED\" type=\"submit\">LED RED</button>");
@@ -126,7 +153,7 @@ void loop(){
             client.println("<button class=\"button\" name=\"LED\" value=\"OFF\" type=\"submit\">LED OFF</button><br><br>");
             client.println("</center></form></body></html>");
             client.println();
-            Serial.println("Base Page Delivered");    
+            Serial.println("Base Page Delivered"); 
             break;
           } 
           else 
